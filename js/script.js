d0ac0f52349bdae516c2a49b61ada9191bce20d3
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2022-08-31 09:04:28 -0300

    FIX-Foram feitas correções nos inputs number para 2 casas decimais

--- js/script.js
+++ js/script.js
@@ -4,39 +4,72 @@ let notaAv1 = $('#nota__av1');
 let notaAv2 = $('#nota__av2');
 let mediaFinal = $('#media__final');
 let aprovadoReprovado = $('#aprovado__reprovado');
+let remover = $('#remover');
+let botaoDelete= $('.botao-delete');
+let btnDeletar = $('#btn-deletar');
+
+
 
 
 let botao = $('#botao')
 
-// Deixar o campo media final escondido
-mediaFinal.parent().hide();
-aprovadoReprovado.parent().hide();
+function camposEscondidos(){
+    // Deixar o campo media final, aprovado/reprovado e idaluno escondido
+    mediaFinal.parent().hide();
+    aprovadoReprovado.parent().hide();
+    idAluno.parent().hide();
+    botaoDelete.hide();
 
-var resultado = (Number(notaAv1.val()) + Number(notaAv2.val())) /2;
+}
 
-botao.on('click', (e) => {
+function funcionalidades(){
+    botao.on('click', (e) => {
+        e.preventDefault();
+        
+        let clone = $('.table-hover').find('tr').eq(1).clone()
+        clone.children('.id').html(idAluno.val())
+        clone.children('.nome').html(nomeAluno.val())
+        clone.children('.av1').html(notaAv1.val())
+        clone.children('.av2').html(notaAv2.val())
+        clone.children('.media-final').html((Number(notaAv1.val()) + Number(notaAv2.val())) /2)
+        clone.children('.aprovado-reprovado').html(aprovadoReprovado.val())
+        clone.children('.remover').html(`<button class="btn btn-primary id="btnDeletar">Deletar</button>`)
+        
+        
+        
+        let media = (Number(notaAv1.val()) + Number(notaAv2.val())) /2
+        
+    
+        if(media >= 6){
+            clone.children('.aprovado-reprovado').html('Aprovado')
+    
+        }else {
+            clone.children('.aprovado-reprovado').html('Reprovado')
+        }
+     
+    
+        $('.table-hover').append(clone)
+        
+        
+        
+    });
+
+}
+
+
+
+
+botaoDelete.on('click', (e) => {
     e.preventDefault();
     
-    let clone = $('.table-hover').find('tr').eq(1).clone()
-    clone.children('.id').html(idAluno.val())
-    clone.children('.nome').html(nomeAluno.val())
-    clone.children('.av1').html(notaAv1.val())
-    clone.children('.av2').html(notaAv2.val())
-    clone.children('.media-final').html((Number(notaAv1.val()) + Number(notaAv2.val())) /2)
-    clone.children('.aprovado-reprovado').html(aprovadoReprovado.val())
-    
-    let media =  (Number(notaAv1.val()) + Number(notaAv2.val())) /2
+    console.log('cliquei')
+})
 
-    if(media >= 6){
-        clone.children('.aprovado-reprovado').html('Aprovado')
+// apagar linha
+function removerLinha(event){
+    event.preventDefault();
+    $(this).parent().parent().remove();
+}
 
-    }else {
-        clone.children('.aprovado-reprovado').html('Reprovado')
-        console.log(media)
-    }
- 
-    
-    $('.table-hover').append(clone)
-    
-});
-
+camposEscondidos();
+funcionalidades();
---
+++ js/teste.js
@@ -0,0 +1,27 @@
+// function temperatura(celsius){
+//     let fah = (celsius * 1.8) + 32;
+//     return fah;
+// }
+
+
+// let testeTemperatura = temperatura(30);
+// console.log(testeTemperatura)
+
+
+// function nome(primeiroNome, segundoNome) {
+//     let nome = primeiroNome
+//     let sobreNome = segundoNome
+//     return nome + ' ' +  '' + sobreNome
+// }
+
+// let nomeCompleto = nome('Carlos', 'Daniel');
+// console.log(nomeCompleto)
+
+
+
+function formulaNotas(notaAv1, notaAv2) {
+    let av1 = Number(notaAv1);
+    let av2 = Number(notaAv2);
+    let media = (av1 + av2) / 2;
+    return media;
+}
